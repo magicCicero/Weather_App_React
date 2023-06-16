ef0003229a1bb3e45c592f3bcedb99d34716fcce
magicCicero <cicerooantonio20@gmail.com>
2023-06-16 10:19:36 +0200

    deleted basename

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,7 @@ import Home from "./pages/Home/Home";
 function App() {
   return (
     <>
-      <BrowserRouter
-        basename={import.meta.env.DEV ? "/" : "/Project_Beer_API/"}
-      >
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
